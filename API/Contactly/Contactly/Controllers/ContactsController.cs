1c0f594f48e5bdadda7b4e8f7abb468221589518
Risenga (RT)  Sono <85702473+SengiSliko@users.noreply.github.com>
2024-12-09 16:22:07 +0200

    APi First Commit

---
+++ API/Contactly/Contactly/Controllers/ContactsController.cs
@@ -0,0 +1,58 @@
+﻿using Contactly.Data;
+using Contactly.Models;
+using Contactly.Models.Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Contactly.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactsController : ControllerBase
+    {
+        private readonly ContactlyDbContext dbContext;
+
+        public ContactsController(ContactlyDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllContacts()
+        {
+           var contacts = dbContext.Contacts.ToList();
+            return Ok(contacts);
+        }
+
+        [HttpPost]
+        public IActionResult AddContact(ContactsDto request)
+        {
+            var domainModelContact = new Contact
+            {
+                Id = Guid.NewGuid(),
+                Name = request.Name,
+                Email = request.Email,
+                Phone = request.Phone,
+                Favourite = request.Favourite
+            };
+            dbContext.Contacts.Add(domainModelContact);
+            dbContext.SaveChanges();
+            return Ok(domainModelContact);
+        }
+
+        [HttpDelete]
+        [Route("{id:guid}")]
+        public IActionResult DeleteContact(Guid id)
+        {
+            var contact = dbContext.Contacts.Find(id);
+
+            if(contact is not null)
+            {
+                dbContext.Contacts.Remove(contact);
+                dbContext.SaveChanges();
+            }
+            return Ok();    
+        }
+    }
+}
---
+++ API/Contactly/Contactly/Models/ContactsDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Contactly.Models
+{
+    public class ContactsDto
+    {
+
+        public required string Name { get; set; }
+        public string? Email { get; set; }
+        public required string Phone { get; set; }
+
+        public bool Favourite { get; set; }
+
+    }
+}
